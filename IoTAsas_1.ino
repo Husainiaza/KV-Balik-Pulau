2ce9d05b7d3f5b095d4eaeb40bf504161ee06033
Husainiaza <148662620+Husainiaza@users.noreply.github.com>
2025-06-19 13:03:42 +0800

    Create IoTAsas_1.ino

---
+++ IoTAsas_1.ino
@@ -0,0 +1,134 @@
+/*------------------------------------------------------------------
+  ===================  Seksyen 1 - HEADER        ===================
+  ------------------------------------------------------------------*/
+
+//-------------------------------------------------------------------
+//=  A. - Library  include and define  yang diperlukan              =
+//-------------------------------------------------------------------
+
+#include <WiFi.h>
+#include <MQTT.h>
+
+//--------------------------------------------------------------------------
+//-----b. - ISTIHAR Sambungan nama Pin dengan Nombor Fizikal PIN ----
+//--------------------------------------------------------------------------
+
+//---Actuator and Relay  pin connection---
+
+#define sensorVoltan   34
+
+
+// ~~~~~~~~~~~~~~~~~~~~ PENTING ~~~~~~~~~~~~~~~~~~~~~~~~
+// ------ Sila edit nama atau ID ikut keperluan --------
+#define Client_Id   "id_anda___"
+#define NamaBroker  "broker.emqx.io"
+#define namaSSID    "IoT";
+#define SSIDpwd     "iot@kksj2023";
+// ~~~~~~~~~~~~~~~~~~~  TAMMAT   ~~~~~~~~~~~~~~~~~~~~~~~
+
+//-----c.  - ISTIHAR  constant dan pembolehubah------------------------------
+//---Penetapan nama Pembolehubah yg diumpukkan kepada satu nilai awal  --
+const char ssid[] = namaSSID;
+const char pass[] = SSIDpwd;
+
+//------ ISTIHAR Pemboleh ubah dengan jenis Data yang digunakan---
+unsigned long lastMillis = 0;
+float dataSuhuC; // suhu dalam Celsius
+int tiltState = 0;
+
+//-----d. - Cipta Objek dari Librari berkaitan------------------ ----
+//--------------------------------------------------------------------------
+
+WiFiClient net;
+MQTTClient client;
+
+
+//##################  Seksyen 1 - TAMAT #############################
+//--------------FUNCTION----------------------------
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect(Client_Id)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nconnected!");
+  client.subscribe("kksbj/buzzer");
+  // -- tambah subcribe disini ---
+ 
+}
+
+void messageReceived(String &topic, String &payload) {
+  //Serial.println("incoming: " + topic + " - " + payload);
+   Serial.println(payload);
+  
+  //==============================================================
+
+  //==========================Relay Control ========================
+  
+
+  
+  
+  //   ----Tulis Kod Kawalan ( subsribe here ) -------
+
+  //--------------------------- Relay Control --------------------
+
+
+}
+
+
+//###################################################
+//==================  Seksyen 2 - Fungsi Setup ======================
+//-------------------------------------------------------------------
+
+void setup() {
+  
+
+  Serial.begin(115200); // initialize serial
+/*
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  } */
+
+  WiFi.begin(ssid, pass);
+
+  client.begin(NamaBroker, net);
+  client.onMessage(messageReceived);
+
+
+  connect();
+
+}
+//##################  Seksyen 2 - TAMAT #############################
+
+
+//==============  Seksyen 3 - Fungsi Utama (LOOP) ===================
+//-------------------------------------------------------------------
+void loop() {
+
+ client.loop();
+ delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!client.connected()) {
+    connect();
+  }
+
+// publish a message roughly every second.
+  if (millis() - lastMillis > 5000) {
+  lastMillis = millis();
+
+    //------SENSOR 1-------------------------------------------------------------
+    
+    
+ //-----------end ----------------------------------
+  }
+}
+
+//##################  Seksyen 3 - TAMAT #############################
